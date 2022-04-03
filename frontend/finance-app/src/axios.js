a7ff2bbef272f6b376e897f6ac311fbdc8fbaea8
Maciej <maciekslawny@gmail.com>
2022-04-03 23:13:26 +0200

    changed ip

--- frontend/finance-app/src/axios.js
+++ frontend/finance-app/src/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 // 51.68.137.244
 // 127.0.0.1:8000
 
-const baseURL = "http://127.0.0.1:8000/api/";
+const baseURL = "http://51.68.137.244/api/";
 
 const axiosInstance = axios.create({
   baseURL: baseURL,
